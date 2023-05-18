01d2920569e1bd6cf44bc4df1e68cd1166ac2d5e
Eirik Klebo <eiriktobias@gmail.com>
2023-05-18 15:34:10 +0200

    milestone2

--- js/main.js
+++ js/main.js
@@ -1,51 +1,94 @@
 class Player {
-    constructor(){
-        this.positionX = 50;
-        this.positionY = 0;
-        this.width = 20;
-        this.height = 20;
+  constructor() {
+    this.positionX = 50;
+    this.positionY = 0;
+    this.width = 20;
+    this.height = 20;
 
-        this.domElement = null; // we will store a ref. to the dom element of the player
+    this.domElement = null; // we will store a ref. to the dom element of the player
 
-        this.createDomElement();
-    }
+    this.createDomElement();
+  }
 
-    createDomElement(){
-        // step1: create the element
-        this.domElement = document.createElement("div");
+  createDomElement() {
+    // step1: create the element
+    this.domElement = document.createElement("div");
 
-        // step2: add content or modify (ex. innerHTML...)
-        this.domElement.id = "player";
-        this.domElement.style.width = this.width + "vw";
-        this.domElement.style.height = this.height + "vh";
-        this.domElement.style.left = this.positionX + "vw";
-        this.domElement.style.bottom = this.positionY + "vh";
+    // step2: add content or modify (ex. innerHTML...)
+    this.domElement.id = "player";
+    this.domElement.style.width = this.width + "vw";
+    this.domElement.style.height = this.height + "vh";
+    this.domElement.style.left = this.positionX + "vw";
+    this.domElement.style.bottom = this.positionY + "vh";
 
-        //step3: append to the dom: `parentElm.appendChild()`
-        const parentElm = document.getElementById("board");
-        parentElm.appendChild(this.domElement);
-    }
+    //step3: append to the dom: `parentElm.appendChild()`
+    const parentElm = document.getElementById("board");
+    parentElm.appendChild(this.domElement);
+  }
 
-    moveLeft(){
-        this.positionX--;
-        this.domElement.style.left = this.positionX + "vw";
-        
-    }
-    moveRight(){
-        this.positionX++;
-        this.domElement.style.left = this.positionX + "vw";
-    }
+  moveLeft() {
+    this.positionX = this.positionX -4; //modify the position
+    this.domElement.style.left = this.positionX + "vw"; //reflect change in the css
+  }
+  moveRight() {
+    this.positionX = this.positionX +4; //modify the position
+    this.domElement.style.left = this.positionX + "vw"; //reflect change in the css
+  }
 }
 
+class Obstacle {
+  constructor() {
+    this.positionX = 50;
+    this.positionY = 100;
+    this.width = 20;
+    this.height = 10;
+
+    this.domElement = null;
+
+    this.createDomElement();
+  }
+  createDomElement() {
+    // step1: create the element
+    this.domElement = document.createElement("div");
+
+    // step2: add content or modify (ex. innerHTML...)
+    this.domElement.className = "obstacle";
+    this.domElement.style.width = this.width + "vw";
+    this.domElement.style.height = this.height + "vh";
+    this.domElement.style.left = this.positionX + "vw";
+    this.domElement.style.bottom = this.positionY + "vh";
+
+    //step3: append to the dom: `parentElm.appendChild()`
+    const parentElm = document.getElementById("board");
+    parentElm.appendChild(this.domElement);
+  }
+  moveDown() {
+    this.positionY--;
+    this.domElement.style.bottom = this.positionY + "vh";
+  }
+}
 
 const player = new Player();
+const obstaclesArr = []; // will store instances of the class Obstacle
 
+// Create new obstacles
+setInterval(() => {
+  const newObstacle = new Obstacle();
+  obstaclesArr.push(newObstacle);
+}, 4000);
 
-// attach event listeners...
+// Move all obstacles
+setInterval(() => {
+  obstaclesArr.forEach((obstacleInstance) => {
+    obstacleInstance.moveDown();
+  });
+}, 60);
+
+// Attach event listeners...
 document.addEventListener("keydown", (event) => {
-    if(event.code === "ArrowLeft"){
-        player.moveLeft();
-    } else if (event.code === "ArrowRight"){
-        player.moveRight();
-    }
-})
+  if (event.code === "ArrowLeft") {
+    player.moveLeft();
+  } else if (event.code === "ArrowRight") {
+    player.moveRight();
+  }
+});
